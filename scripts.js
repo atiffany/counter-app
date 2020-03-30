18a397e963c17b54bc00c944139749618f809760
atiffany <abby.tiffany@gmail.com>
2020-03-30 21:22:12 +0200

    Print numbers from user input to the console

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
-function greet() {
-  console.log('greeting initiated');
-  alert('Hi!');
+function numberPrinter(event) {
+  event.preventDefault();
+  let input = document.getElementById("input").value;
+  console.log(input);
+  let orderedNumbers = [];
+  for (let i = 1; i <= parseInt(input); i++) {
+    orderedNumbers.push(i);
+  }
+  console.log(orderedNumbers);
 }
